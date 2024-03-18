30a47723e84e3b455ff7c64ca1bb7c1bbb157af5
Gabriel Evaristo <gabrielousadia12@gmail.com>
2024-03-18 09:51:54 -0300

    Atv 2 done
    Atividade 2 completa

---
+++ EDAA ATV3/src/Atv2/Main.java
@@ -0,0 +1,38 @@
+package Atv2;
+
+import Atv2.Sort.Sort;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void printCompare(ArrayList<String> nameList, ArrayList<String> ordNameList){
+        System.out.println("\nLista Desordenada");
+        System.out.println("--------------");
+        for(String name  : nameList){
+            System.out.println(name);
+        }
+        System.out.println("\nLista Ordenada");
+        System.out.println("--------------");
+        for(String name: ordNameList){
+            System.out.println(name);
+        }
+        System.out.println("--------------");
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int entry;
+        ArrayList<String> names = new ArrayList<>();
+        System.out.println("Quantos nomes deseja digitar?");
+        entry = sc.nextInt();
+        for(int i = 0; i < entry; i++){
+            System.out.printf("Nome #%d: ", i);
+            String name = sc.next();
+            names.add(name);
+        }
+        ArrayList<String> namesOrd = new ArrayList<>(names);
+        Sort.namesInsertionSort(namesOrd);
+        printCompare(names, namesOrd);
+
+    }
+}
---
+++ EDAA ATV3/src/Atv2/Sort/Sort.java
@@ -0,0 +1,38 @@
+package Atv2.Sort;
+
+
+import java.util.ArrayList;
+
+public class Sort {
+    public static char[] stringToArray(String s){
+        char[] arr = new char[s.length()];
+        for(int i =0; i < s.length(); i++){
+            arr[i] = s.toLowerCase().charAt(i);
+        }
+        return arr;
+    }
+    public static int compareNameChar(String name, String name2){
+        char[] arr1 = stringToArray(name);
+        char[] arr2 = stringToArray(name2);
+
+        for(int i = 0; i < arr1.length; i++){
+            if((int)arr1[i] < (int)arr2[i]){
+                return -1;
+            }else if((int)arr1[i] > (int)arr2[i]){
+                return 1;
+            }
+        }
+        return 0;
+    }
+    public static void namesInsertionSort(ArrayList<String> nameList){
+        for(int i = 1; i < nameList.size(); i++){
+            String temp = nameList.get(i);
+            int j = i - 1;
+            while(j >= 0 && compareNameChar(nameList.get(j), temp) > 0){
+                nameList.set(j+1, nameList.get(j));
+                j--;
+            }
+            nameList.set(j+1,temp);
+        }
+\    }
+}
