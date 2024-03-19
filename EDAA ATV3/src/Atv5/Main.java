29cc210c8b9088390cc1f5366e51e8bc251adb3c
Gabriel Evaristo <gabrielousadia12@gmail.com>
2024-03-19 08:48:56 -0300

    Atv5 Done
    Atividade 5 completa

---
+++ EDAA ATV3/src/Atv5/Main.java
@@ -0,0 +1,26 @@
+package Atv5;
+
+import Atv5.Sort.Sort;
+
+import java.util.Scanner;
+
+public class Main {
+    public static char[] stringToArray(String s){
+        char[] arr = new char[s.length()];
+        for(int i =0; i < s.length(); i++){
+            arr[i] = s.toLowerCase().charAt(i);
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char[] phraseCharList;
+        System.out.print("Digite uma frase: ");
+        String phrase = sc.next();
+        phraseCharList = stringToArray(phrase);
+        System.out.println("--------------");
+        System.out.println("Organizado: ");
+        Sort.quickSort(phraseCharList);
+        System.out.println(phraseCharList);
+    }
+}
---
+++ EDAA ATV3/src/Atv5/Sort/Sort.java
@@ -0,0 +1,47 @@
+package Atv5.Sort;
+
+import java.util.ArrayList;
+
+public class Sort {
+    public static int compareChar(char c1, char c2) {
+        if ((int) c1 < c2) {
+            return -1;
+        } else if ((int) c1 > (int) c2) {
+            return 1;
+        }
+        return 0;
+    }
+
+    public static void quickSort(char[] charList) {
+        ordena(charList, 0, charList.length - 1);
+    }
+
+    private static void ordena(char[] list, int esq, int dir) {
+        if (esq < dir) {
+            int[] p = particao(list, esq, dir);
+            ordena(list, esq, p[1]);
+            ordena(list, p[0], dir);
+        }
+    }
+
+    private static int[] particao(char[] list, int esq, int dir) {
+        int i = esq, j = dir;
+        char x = list[(i + j) / 2];
+        while (i <= j) {
+            while (list[i] < x) {
+                i++;
+            }
+            while (list[j] > x) {
+                j--;
+            }
+            if (i <= j) {
+                char temp = list[i];
+                list[i] = list[j];
+                list[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        return new int[]{i, j};
+    }
+}
